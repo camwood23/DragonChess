9d004f40ad4858b3e901c3d6faf9353b8007256c
camwood23 <camwood23@gmail.com>
2015-06-29 17:18:59 -0400

    Have three grids now

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 //Put javascript/jquery code here
-document.write("<table>");
-for (var i = 0; i < 8; i++) {
-    document.write("<tr>");
-    for (var j = 0; j < 12; j++) {
-        document.write("<td><div></div></td>");
+function makeTable(id) {
+    document.write("<table>");
+    for (var i = 0; i < 8; i++) {
+        document.write("<tr>");
+        for (var j = 0; j < 12; j++) {
+            document.write("<td><div id='" + id + "'></div></td>");
+        }
+        document.write("</tr>");
     }
-    document.write("</tr>");
+    document.write("</table>");
 }
-document.write("</table>");
+
+makeTable("sky");
+makeTable("ground");
+makeTable("subterrain");
 
 $(document).ready(function() {
     //put dynamic code inside this function
