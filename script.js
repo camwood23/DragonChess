86dffac91779aa91642beb96ff8c62475c92bdc5
camwood23 <camwood23@gmail.com>
2015-06-29 16:56:05 -0400

    Added a grid

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //Put javascript/jquery code here
+document.write("<table>");
+for (var i = 0; i < 8; i++) {
+    document.write("<tr>");
+    for (var j = 0; j < 12; j++) {
+        document.write("<td><div></div></td>");
+    }
+    document.write("</tr>");
+}
+document.write("</table>");
+
 $(document).ready(function() {
     //put dynamic code inside this function
 });
